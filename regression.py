2df40c0771eab29c620cfd3d6f0002542ecf238b
hapoore <hapoore@stanford.edu>
2016-12-11 13:58:10 -0800

    added code to extract age and team defense features from the war file. Sort of inefficient, but data set is small.

--- regression.py
+++ regression.py
@@ -1,7 +1,10 @@
 import csv
 import numpy as np
+import optunity
+import optunity.metrics
 from sklearn.svm import SVR
 from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.cross_validation import cross_val_score
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import SGDRegressor
@@ -55,6 +58,25 @@ def getStats():
                     for i in range(len(headers)):
                         player[headers[i]] = row[i]
                     players.append(player)
+
+    new_headers = []
+    with open('war_daily_pitch.txt', 'rb') as war_file:
+        war_reader = csv.reader(war_file)
+        for row in war_reader:
+            if len(new_headers) == 0:
+                new_headers = row
+            else:
+                year = int(row[4])
+                player_id = row[3]
+                if year < YEAR or rookieYears.get(player_id) != year:
+                    continue
+                runs_saved = float(row[22])
+                age = int(row[1])
+                for player in players:
+                    if player['playerID'] != player_id:
+                        continue
+                    player['age'] = age
+                    player['RS_def_total'] = runs_saved
     return players, rookieYears
 
 def getFeatureDict(player):
@@ -72,7 +94,12 @@ def getFeatureDict(player):
     features['HR'] = int(player['HR'])
     features['BB'] = int(player['BB'])
     features['BB/IP'] = features['BB']/ip
-    # features['BAOpp'] = float(player['BAOpp'])
+    features['RS_def_total'] = player['RS_def_total']
+    features['age'] = player['age']
+    if('.' not in player['BAOpp']):
+        features['BAOpp'] = 0.0
+    else:
+        features['BAOpp'] = float(player['BAOpp'])
     features['R'] = int(player['R'])
     features['ER'] = int(player['ER'])
     features['rookieWAR'] = player['rookieWAR']
@@ -94,6 +121,39 @@ def getPlayersFutureWar(players, rookieYears, warYears):
                 data.append(player)
     print len(data)
     return data
+
+def getOversampledData(x, y):
+    ratio = len(y)/sum(y)
+    new_x = []
+    new_y = []
+    for i in range(ratio):
+        for index in range(len(y)):
+            if y[index] == 1:
+                new_x.append(x[index])
+                new_y.append(1)
+    for data in x:
+        new_x.append(data)
+    for label in y:
+        new_y.append(label)
+    return new_x, new_y
+
+def getUndersampledData(x, y):
+    ratio = len(y)/sum(y)
+    new_x = []
+    new_y = []
+    counter = 0
+    for index in range(len(y)):
+        if y[index] == 0:
+            counter+=1
+            if(counter % ratio == 0):
+                new_x.append(x[index])
+                new_y.append(0)
+        else:
+            new_x.append(x[index])
+            new_y.append(1)
+    return new_x, new_y
+
+
 
 # def squared_loss(feature_list, war, reg, scaler, vec):
 #     loss = 0.0
@@ -146,41 +206,69 @@ for player in data:
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size=.20, random_state=0)
 
+svc = SVC(C=1000, gamma=1e-1, class_weight='balanced')
+classes = assignClassLabels(y_train)
+
+x_train_over, classes_over = getOversampledData(x_train, classes)
+x_train_under, classes_under = getUndersampledData(x_train, classes)
+
+gbc = GradientBoostingClassifier()
+
 vec = DictVectorizer()
 scaler = StandardScaler()
 
-svc = SVC()
-classes = assignClassLabels(y_train)
-
-vec.fit_transform(x_train).toarray()
-scores = cross_val_score(svc, vec.transform(x_train).toarray(), classes)
+scaler.fit(vec.fit_transform(x_train).toarray())
+scores = cross_val_score(gbc, scaler.transform(vec.transform(x_train).toarray()), classes, cv=5)
 print scores
-svc.fit(vec.transform(x_train).toarray(), classes)
+scores = cross_val_score(gbc, scaler.transform(vec.transform(x_train_over).toarray()), classes_over, cv=5)
+print scores
+scores = cross_val_score(gbc, scaler.transform(vec.transform(x_train_under).toarray()), classes_under, cv=5)
+print scores
+svc.fit(scaler.transform(vec.transform(x_train).toarray()), classes)
+gbc.fit(scaler.transform(vec.transform(x_train).toarray()), classes)
+
+print len(classes_over)
+print sum(classes_over)
+
+print len(classes_under)
+print sum(classes_under)
+
+# index = 0
+# count = 0
+# for feature in x_train:
+#     val = svc.predict(scaler.transform(vec.transform(feature).toarray()))
+#     if(val[0] != classes[index]):
+#         print val[0]
+#         count += 1
+#     index += 1
+# print count
+
+# index = 0
+# count = 0
+# for feature in x_train:
+#     val = gbc.predict(scaler.transform(vec.transform(feature).toarray()))
+#     if(val[0] != classes[index]):
+#         print val[0]
+#         count += 1
+#     index += 1
+# print count
+# print(sum(classes))
+
 
 index = 0
 count = 0
-for feature in vec.transform(x_train).toarray():
-    val = svc.predict(feature)
-    if(val[0] != classes[index]):
+test_classes = assignClassLabels(y_test)
+for feature in x_test:
+    val = svc.predict(scaler.transform(vec.transform(feature).toarray()))
+    if(val[0] != test_classes[index]):
         print val[0]
         count += 1
     index += 1
 
-print count
-
-# index = 0
-# count = 0
-# test_classes = assignClassLabels(y_test)
-# for feature in vec.transform(x_test).toarray():
-#     val = svc.predict(feature)
-#     if(val[0] != test_classes[index]):
-#         print val[0]
-#         count += 1
-#     index += 1
-
 # print test_classes
 # print(sum(test_classes))
-# print count
+print index
+print count
 
 
 
